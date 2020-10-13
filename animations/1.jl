8553700f44087c0695448352301c5445371d5a19
Datseris <datseris.george@gmail.com>
2020-10-13 15:35:48 +0200

    display poincare section of Lorenz system

--- animations/1.jl
+++ animations/1.jl
@@ -2,30 +2,58 @@
 using DrWatson
 @quickactivate "ExercisesRepo"
 include(srcdir("colorscheme.jl"))
+
 # Animate evolution of trajectories in Lorenz
+using InteractiveChaos
+using DynamicalSystems, Makie
+using OrdinaryDiffEq
 
 ds = Systems.lorenz()
 
 u1 = [10,20,40.0]
-u2 = [10,20,40.0 + 1e-6]
+u2 = [10,20,40.0 + 1e-3]
 u3 = [20,10,40.0]
 u0s = [u1, u2, u3]
-lims = ((-25, 25), (-25, 25), (0, 40))
 
-somescene, someobs = interactive_evolution(
-    ds, u0s; idxs = SVector(1, 2, 3), dtmax = 0.001, tail = 1000,
-    lims = lims
+diffeq = (alg = Tsit5(), dtmax = 0.01)
+
+scene, main, layout, obs = interactive_evolution(
+    ds, u0s; tail = 10000, diffeq, colors = COLORSCHEME,
 )
+
+# %% Add poincare plane
+using AbstractPlotting.MakieLayout
+
+o = Point3f0(-25, 0, 0)
+w = Point3f0(50, 0, 50)
+p = FRect3D(o, w)
+# mesh!(main, p; color = RGBAf0(0.2, 0, 0.8), transparent = true)
+
+# These lines are necessary for transparent planes
+a = RGBAf0(0,0,0,0)
+c = RGBAf0(0.2, 0.2, 0.8, 1.0)
+img = AbstractPlotting.ImagePattern([c a; a c]); # This throws an error if it shows, you can ignore that
+mesh!(main, p; color = img)
+
+# Plot Poincare sos
+psosplot = layout[:, 2] = LAxis(scene)
+psos = poincaresos(ds, (2, 0.0), 2000.0)
+scatter!(psosplot, psos[:, 1], psos[:, 3])
+
+display(scene)
 
 # %%
-ds = Systems.towel()
-u1 = rand(3)
-u2 = rand(3)
-u3 = rand(3)
-u0s = [u1, u2, u3]
-lims = ((-1, 1), (-0.1, 0.1), (-1, 2))
+ds = Systems.henonheiles()
 
-somescene, someobs = interactive_evolution(
-    ds, u0s; idxs = SVector(1, 2, 3), dtmax = 0.001, tail = 100000,
-    lims = lims
+u0s = [[0.0, -0.25, 0.42081, 0.0],
+[0.0, 0.1, 0.5, 0.0],
+[0.0, -0.31596, 0.354461, 0.0591255]]
+
+diffeq = (alg = Vern9(), dtmax = 0.01)
+idxs = (1, 2, 4)
+colors = ["#233B43", "#499cbf", "#E84646"]
+
+scene, main, layout, obs = interactive_evolution(
+    ds, u0s; idxs, tail = 2000, diffeq, colors
 )
+main.scene[Axis][:names, :axisnames] = ("q₁", "q₂", "p₂")
