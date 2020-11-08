a957c1020ccdc7eaa391755f80e7438255ecc43d
Datseris <datseris.george@gmail.com>
2020-11-08 13:46:05 +0100

    add MC for excitable system

--- animations_code/2.jl
+++ animations_code/2.jl
@@ -5,19 +5,12 @@ using Makie, DynamicalSystems, InteractiveChaos
 using AbstractPlotting.MakieLayout
 
 # %% Animation of changing ε in curve of 1D energy balance model
-function fitzhugh(x, p, t)
-    u, w = x
-    I, a, b = p
-    udot = a*u*(u-b)*(1-u) - w + I
-    wdot = 0.01*(u-w)
-    return SVector(udot, wdot)
-end
-
-a = 3.0
+a = 3.
 b = 0.2
+ε = 0.01
 I = 0.0
-p = [I, a, b]
-fh = ContinuousDynamicalSystem(fitzhugh, rand(2), p)
+
+fh = Systems.fitzhugh_nagumo(zeros(2); a, b, ε, I)
 
 # Layout plot:
 scene, layout = layoutscene(resolution = (1000, 800))
@@ -33,10 +26,11 @@ w2 = @. a*us*(us-b)*(1-us) + I
 lines!(ax, us, w1; color = COLORS[1], linewidth = 2, linestyle = :dash)
 lines!(ax, us, w2; color = COLORS[2], linewidth = 2, linestyle = :dot)
 
+# Create trajectories on point selection
 spoint = select_point(ax.scene)
 on(spoint) do pos
     tr = trajectory(fh, 10000, SVector(pos...))
-    lines!(ax.scene, columns(tr)...; color = InteractiveChaos.randomcolor())
+    lines!(ax, columns(tr)...; color = InteractiveChaos.randomcolor())
 end
 
 # %% orbit evolution in a 2D torus
@@ -64,12 +58,14 @@ function quasiperiodic_f(u, p, t)
 end
 
 ds = ContinuousDynamicalSystem(quasiperiodic_f, [0.0, 0.0, 0.0], nothing)
-u0s = [[0, 0, ω] for ω ∈ [3, sqrt(7), 5]]
+u0s = [[0, 0, ω] for ω ∈ [sqrt(7), 3]]
 diffeq = (alg = Tsit5(), dtmax = 0.01)
 
 lims = ((-R-r, R+r), (-R-r, R+r), (-3r, 3r))
 
+plotkwargs = [(linewidth = i^2*1.0,) for i in 1:length(u0s)]
+
 scene, main, layout, obs = interactive_evolution(
     ds, u0s; tail = 20000, diffeq, colors = COLORS, transform = torus, lims,
-    plotkwargs = (linewidth = 1.0,),
+    plotkwargs
 )
---
+++ animations_code/4.jl
@@ -0,0 +1,2 @@
+# TODO:
+# slider changing the curve of the 1D climate model
